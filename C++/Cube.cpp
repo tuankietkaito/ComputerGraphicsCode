05e8124b8deb2f7e9b6f0ef19314c7ea9494f49f
tuankietkaito <tuankietkaito@gmail.com>
2021-03-15 18:46:41 +0700

    Cube

---
+++ C++/Cube.cpp
@@ -0,0 +1,82 @@
+#include <windows.h>
+#include <GL/glut.h>
+
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(3, -3, 3, 0, 0, 0, 0, 1, 0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glBegin(GL_QUADS);					// Begin drawing the color cube with 6 quads
+										// Top face (y = 1.0f)
+										// Define vertices in counter-clockwise (CCW) order with normal pointing out
+		glColor3f(0.0f, 1.0f, 0.0f);     // Green	
+		glVertex3f(1.0f, 1.0f, -1.0f);
+		glVertex3f(-1.0f, 1.0f, -1.0f);
+		glVertex3f(-1.0f, 1.0f, 1.0f);
+		glVertex3f(1.0f, 1.0f, 1.0f);
+
+		// Bottom face (y = -1.0f)
+		glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+		glVertex3f(1.0f, -1.0f, 1.0f);
+		glVertex3f(-1.0f, -1.0f, 1.0f);
+		glVertex3f(-1.0f, -1.0f, -1.0f);
+		glVertex3f(1.0f, -1.0f, -1.0f);
+
+		// Front face  (z = 1.0f)
+		glColor3f(1.0f, 0.0f, 0.0f);     // Red
+		glVertex3f(1.0f, 1.0f, 1.0f);
+		glVertex3f(-1.0f, 1.0f, 1.0f);
+		glVertex3f(-1.0f, -1.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, 1.0f);
+
+		// Back face (z = -1.0f)
+		glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+		glVertex3f(1.0f, -1.0f, -1.0f);
+		glVertex3f(-1.0f, -1.0f, -1.0f);
+		glVertex3f(-1.0f, 1.0f, -1.0f);
+		glVertex3f(1.0f, 1.0f, -1.0f);
+
+		// Left face (x = -1.0f)
+		glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+		glVertex3f(-1.0f, 1.0f, 1.0f);
+		glVertex3f(-1.0f, 1.0f, -1.0f);
+		glVertex3f(-1.0f, -1.0f, -1.0f);
+		glVertex3f(-1.0f, -1.0f, 1.0f);
+
+		// Right face (x = 1.0f)
+		glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+		glVertex3f(1.0f, 1.0f, -1.0f);
+		glVertex3f(1.0f, 1.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, -1.0f);
+
+	glEnd();  // End of drawing color-cube
+
+	glFlush();
+}
+
+void init() {
+	glFrontFace(GL_CCW);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-3, 3, -3, 3, -7, 7);
+}
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("Cube");
+
+	init();
+
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+	return 0;
+}
